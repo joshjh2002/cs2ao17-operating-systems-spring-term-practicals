e036f444c93204b09683f0337efaa25e07da867c
yb012845 <yb012845@student.reading.ac.uk>
2022-03-03 15:32:09 +0000

    Better comments

--- Questions/vaddr2.c
+++ Questions/vaddr2.c
@@ -3,15 +3,16 @@
 int main(int argc, char *argv[])
 {
     unsigned long page, offset, address;
-    if(argc != 2)  exit(1);
-    address= atoll(argv[1]);
+    if (argc != 2)
+        exit(1);
+    address = atoll(argv[1]);
 
-    page = address >> 20;         /*calculating pages number*/
-    offset = address & 0xfffff;     /*calculating remaining offset*/
+    page = address >> 20;       /*calculating pages number: 2^20 = 1 million*/
+    offset = address & 0xfffff; /*calculating remaining offset: 0xfffff is a large buffer.
+                                    It can hold a lot of different values*/
 
     printf("The address %lu contains: \n", address);
-    printf("page number = %lu\n",page);
+    printf("page number = %lu\n", page);
     printf("offset = %lu\n", offset);
     return 0;
 }
-
